ab4e057767b20501b84ccb7532af7a5e25ec085e
Ali A <alsamawiali4@gmail.com>
2022-01-18 15:06:50 -0500

    A code example where the value of the variable amount is unpredictable

--- tech.strategio.helloworld2/src/tech/strategio/helloworld2/Main.java
+++ tech.strategio.helloworld2/src/tech/strategio/helloworld2/Main.java
@@ -1,13 +1,29 @@
 package tech.strategio.helloworld2;
 
 
+//public class Main extends Thread {
+//	  public static void main(String[] args) {
+//	    Main thread = new Main();
+//	    thread.start();
+//	    System.out.println("This code is outside of the thread");
+//	  }
+//	  public void run() {
+//	    System.out.println("This code is running in a thread");
+//	  }
+//	}
+
 public class Main extends Thread {
+	  public static int amount = 0;
+
 	  public static void main(String[] args) {
 	    Main thread = new Main();
 	    thread.start();
-	    System.out.println("This code is outside of the thread");
+	    System.out.println(amount);
+	    amount++;
+	    System.out.println(amount);
 	  }
+
 	  public void run() {
-	    System.out.println("This code is running in a thread");
+	    amount++;
 	  }
 	}
