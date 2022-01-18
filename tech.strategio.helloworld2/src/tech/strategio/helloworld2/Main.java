9b412a8442c37ec1e22616d783a07a8db2bccb68
Ali A <alsamawiali4@gmail.com>
2022-01-18 15:09:14 -0500

    isAlive

--- tech.strategio.helloworld2/src/tech/strategio/helloworld2/Main.java
+++ tech.strategio.helloworld2/src/tech/strategio/helloworld2/Main.java
@@ -12,18 +12,51 @@ package tech.strategio.helloworld2;
 //	  }
 //	}
 
+//public class Main extends Thread {
+//	  public static int amount = 0;
+//
+//	  public static void main(String[] args) {
+//	    Main thread = new Main();
+//	    thread.start();
+//	    System.out.println(amount);
+//	    amount++;
+//	    System.out.println(amount);
+//	  }
+//
+//	  public void run() {
+//	    amount++;
+//	  }
+//	}
+//Use isAlive() to prevent concurrency problems:
+
 public class Main extends Thread {
 	  public static int amount = 0;
 
 	  public static void main(String[] args) {
 	    Main thread = new Main();
 	    thread.start();
-	    System.out.println(amount);
-	    amount++;
-	    System.out.println(amount);
+	    // Wait for the thread to finish
+	    while(thread.isAlive()) {
+	    System.out.println("Waiting...");
 	  }
-
+	  // Update amount and print its value
+	  System.out.println("Main: " + amount);
+	  amount++;
+	  System.out.println("Main: " + amount);
+	  }
 	  public void run() {
 	    amount++;
 	  }
 	}
+
+
+
+
+
+
+
+
+
+
+
+
