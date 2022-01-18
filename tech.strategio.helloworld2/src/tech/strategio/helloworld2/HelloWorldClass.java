4f7fb8d647687bc2f2b1dc87979da631430a851c
Ali A <alsamawiali4@gmail.com>
2022-01-18 14:40:32 -0500

    work?

--- tech.strategio.helloworld2/src/tech/strategio/helloworld2/HelloWorldClass.java
+++ tech.strategio.helloworld2/src/tech/strategio/helloworld2/HelloWorldClass.java
@@ -4,7 +4,7 @@ public class HelloWorldClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello my freinds");
+		System.out.println("Hello my freinds, welcome");
 
 	}
 
